5d145d5de892d4cd93d445c0140885cf12b758a6
pduchnovsky <54581833+pduchnovsky@users.noreply.github.com>
2023-12-26 16:11:39 +0100

    updated

--- static/mon/stp.sh
+++ static/mon/stp.sh
@@ -5,7 +5,7 @@ apt remove --purge "libreoffice-*" vlc-data -y; apt clean -y; apt autoremove -y
 wget https://download.nomachine.com/download/8.10/Linux/nomachine_8.10.1_1_amd64.deb
 dpkg -i nomachine_8.10.1_1_amd64.deb; rm nomachine_8.10.1_1_amd64.deb
 echo "CreateDisplay 0" >> /usr/NX/etc/server.cfg
-su - pd -c 'firefox --display=:0 -CreateProfile pd'
+su - pd -c 'DISPLAY=:0 firefox -CreateProfile pd'
 for i in $(ls -d /home/pd/snap/firefox/common/.mozilla/firefox/* | grep .pd$); do
     mkdir $i/chrome
     echo '@-moz-document url-prefix(https://pdu.i234.me) {
@@ -19,14 +19,15 @@ for i in $(ls -d /home/pd/snap/firefox/common/.mozilla/firefox/* | grep .pd$); d
             display: none !important;
         }
     }' > $i/chrome/userContent.css
-    wget https://github.com/arkenfox/user.js/raw/master/user.js -P $i
+    wget https://github.com/pyllyukko/user.js/raw/relaxed/user.js -P $i
     grep "toolkit.legacyUserProfileCustomizations.stylesheets" "$i/user.js" || echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> "$i/user.js"
     chown -R pd:pd $i
 done
 echo 'xset -dpms s off >/dev/null 2>&1
 pkill -f firefox >/dev/null 2>&1; rm /home/pd/snap/firefox/common/.mozilla/firefox/*/*lock >/dev/null 2>&1
 (ps aux | grep firefox | grep SurveillanceStation) || (firefox --display=:0 -P pd -kiosk https://pdu.i234.me:5001/webman/3rdparty/SurveillanceStation >/dev/null 2>&1) &
-' > /home/pd/.bash_profile
+' > /home/pd/.bash_profile && chown pd:pd /home/pd/.bash_profile
+echo "Hidden=true" >> /etc/xdg/autostart/upg-notifier-autostart.desktop
 { echo '0 5 * * * su - pd -c "./.bash_profile &" &>/dev/null </dev/null'; } | crontab -u root -
 mkdir .ssh; chmod 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys
 echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMpwOis2kDy3KursJmtLLydEqHb87D6+ixTADi7myw8e pd@d-server
@@ -35,4 +36,5 @@ su - pd -c 'mkdir .ssh; chmod 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys'
 echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAp96owJJPXX0o8o7gc6XRqpYGZMAqNpRRVGwJluK6vm pd@any
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMpwOis2kDy3KursJmtLLydEqHb87D6+ixTADi7myw8e pd@d-server
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFjDMWXdYjxWMZMLHp1Tn38E0ahe8uaQ/eEGdcnnu4SF hass' > /home/pd/.ssh/authorized_keys
+echo 'r () { su - pd -c "./.bash_profile &" &>/dev/null </dev/null; }' > .bash_aliases
 sleep 5 && reboot
